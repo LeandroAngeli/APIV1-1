29cd8d56d203df87c90e5d3c375980321269e5a9
GaianiMagali <magaliantonella.gaiani@gmail.com>
2020-05-20 21:46:32 -0300

    Primera parte completa

---
+++ api/routes/materias.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const models = require('../models');
+
+router.get('/', (req, res) => {
+    models.materia.findAll({
+        attributes: ['nombre']
+    })
+        .then(materias => res.send(materias))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send("Algo salio mal!!")
+        });
+});
+
+router.post('/nuevaMateria', (req, res) => {
+    const { nombre, id_carrera } = req.body;
+    models.materia
+        .findOne({
+            attributes: ["id", "nombre"],
+            where: { nombre }
+        }).then(mat => mat ? res.status(400).send({ message: 'Bad request: existe otra materia con el mismo nombre' }) :
+            models.materia
+                .create({ nombre: req.body.nombre })
+                .then(materia => res.status(200).send(materia.nombre))
+        ).catch((error) => {
+            console.log(`Error al intentar insertar en la base de datos: ${error}`)
+            res.sendStatus(500)
+        })
+});
+
+const findMateriaId = (id, { onSuccess, onNotFound, onError }) => {
+    models.materia
+        .findOne({
+            attributes: ["id", "nombre"],
+            where: { id }
+        })
+        .then(materia => (materia ? onSuccess(materia) : onNotFound()))
+        .catch(() => onError());
+};
+
+router.get('/:id', (req, res) => {
+    findMateriaId(req.params.id, {
+        onSuccess: materia => res.send(materia),
+        onNotFound: () => res.sendStatus(404),
+        onError: () => res.sendStatus(500)
+    });
+});
+
+router.put('/actualizarMateria/:id', (req, res) => {
+    const onSuccess = materia => {
+        models.materia.findOne({
+            where: { nombre: req.body.nombre }
+        })
+            .then(mat => mat ? res.status(400).send({ message: 'Bad request: existe otra materia con el mismo nombre' }) :
+                materia.update({ nombre: req.body.nombre }, { fields: ["nombre"] })
+                    .then(response => res.send(response))
+            )
+            .catch(error => console.log(error))
+    }
+
+    findMateriaId(req.params.id, {
+        onSuccess,
+        onNotFound: () => res.sendStatus(404),
+        onError: (error) => {
+            console.log(`Error: ${error}`)
+            res.sendStatus(500)
+        }
+    })
+});
+
+router.delete('/borrarMateria/:id', (req, res) => {
+    const onSuccess = materia =>
+        materia
+            .destroy()
+            .then(() => res.status(200).send(`${materia.nombre} eliminada`))
+            .catch(() => res.sendStatus(500));
+    findMateriaId(req.params.id, {
+        onSuccess,
+        onNotFound: () => res.sendStatus(404),
+        onError: () => res.sendStatus(500)
+    })
+})
+
+module.exports = router;
