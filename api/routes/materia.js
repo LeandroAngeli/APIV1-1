d405884d3c68103b7bbf730066708d7a84a0c1e9
kezgan <lautaromcasella@gmail.com>
2020-05-25 23:21:11 -0300

    Nuevo branch

--- api/routes/materia.js
+++ api/routes/materia.js
@@ -6,7 +6,7 @@ router.get("/", (req, res) => {
   console.log("Esto es un mensaje para ver en consola");
   models.materia
     .findAll({
-      attributes: ["id", "nombre", "idcarrera"]
+      attributes: ["id", "nombre", "id_carrera"]
     })
     .then(materia => res.send(materia))
     .catch(() => res.sendStatus(500));
